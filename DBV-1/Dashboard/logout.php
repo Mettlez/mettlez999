baa33ae00e0670bf1a0d1ba7b877a14785fbea2b
Mettlez <161104537+Mettlez@users.noreply.github.com>
2025-03-11 15:27:48 +0700

    Delete DBV-1 directory

--- DBV-1/Dashboard/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); // เริ่ม session
-session_unset(); // ลบตัวแปร session ทั้งหมด
-session_destroy(); // ทำลาย session
-
-// ส่งคำตอบกลับให้ JavaScript
-echo json_encode(["status" => "success"]);
-?>
